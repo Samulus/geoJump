5eaa39f22ced023cfd2e704971180718651e3d61
Doctor-Samulus <samuelalexandervargas@gmail.com>
2012-03-06 22:17:15 +0000

    changed title positions

--- title.lua
+++ title.lua
@@ -1,9 +1,9 @@
 title = gs.new()
 function title:draw()
 	love.graphics.setFont(fntA)
-	love.graphics.print("geoMan", 400, 500)
+	love.graphics.print("geoJump", 500, 500)
 	love.graphics.setFont(fntB)
-	love.graphics.print("hit enter to begin", 430, 535)
+	love.graphics.print("hit enter to begin", 550, 550)
 end
 
 function title:keypressed(button)
