6442915ab95074bad92f833e15f2bbbca7e130f6
Doctor-Samulus <samuelalexandervargas@gmail.com>
2012-03-06 22:13:44 +0000

    cosmetic changes

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
--- main.lua
-gs = require "lib/hump/gamestate"
-require "title"
-require "menu" 
-require "make"
+gs = require ("lib/hump/gamestate")
+gui = require ("lib/quickie")
+
+require ("title")
+require ("menu")
+require ("edit")
+
 function love.load()
 	fntA = love.graphics.newFont("gfx/fntA.ttf", 22)
 	fntB = love.graphics.newFont("gfx/fntB.ttf", 14)
